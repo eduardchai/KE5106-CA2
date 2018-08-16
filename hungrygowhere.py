283ec2e43dab92609cc226fd7505e16b7bbb7ec3
Eduard Chai <eduard.chai@salestock.id>
2018-08-16 08:22:40 +0800

    store data ini mongodb

--- hungrygowhere.py
+++ hungrygowhere.py
@@ -2,20 +2,31 @@ import os
 import unicodedata
 import pprint
 import time
+import json
+import re
+import requests
 from bs4 import BeautifulSoup as soup
 from selenium import webdriver
 from selenium.webdriver.common import action_chains
 from selenium.webdriver.common.keys import Keys  
 from selenium.webdriver.chrome.options import Options
+from pymongo import MongoClient
 
 whitelist = ["Instant Booking", "Address"]
 base_url = "https://www.hungrygowhere.com"
-limit = 3
+limit = 9999
+
+client = MongoClient('', #ip_address:port
+                     username='', #username
+                     password='', #password
+                     authSource='admin',
+                     authMechanism='SCRAM-SHA-1')
 
 def get_browser(driver=None):
     if driver == "chrome":
         chrome_options = Options()  
-        # chrome_options.add_argument("--headless")  
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")  
         chrome_options.binary_location = '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'
 
         return webdriver.Chrome(executable_path=os.path.abspath("chromedriver"), chrome_options=chrome_options)
@@ -28,109 +39,106 @@ def get_browser(driver=None):
 def normalize(text):
     return unicodedata.normalize("NFKD", text).strip()
 
-def review_page(url):
+def get_rating_value(star_view_el):
+    rating = 0
+    if star_view_el.find("span", "sprite star-voted star-05"):
+        rating = 0.5
+    elif star_view_el.find("span", "sprite star-voted star-1"):
+        rating = 1
+    elif star_view_el.find("span", "sprite star-voted star-15"):
+        rating = 1.5
+    elif star_view_el.find("span", "sprite star-voted star-2"):
+        rating = 2
+    elif star_view_el.find("span", "sprite star-voted star-25"):
+        rating = 2.5
+    elif star_view_el.find("span", "sprite star-voted star-3"):
+        rating = 3
+    elif star_view_el.find("span", "sprite star-voted star-35"):
+        rating = 3.5
+    elif star_view_el.find("span", "sprite star-voted star-4"):
+        rating = 4
+    elif star_view_el.find("span", "sprite star-voted star-45"):
+        rating = 4.5
+    elif star_view_el.find("span", "sprite star-voted star-5"):
+        rating = 5
+
+    return rating
+
+def review_page(url, restaurant_id):
+    db = client["KE5106"]
+
     browser = get_browser("chrome")
     browser.get(url)
-    action = action_chains.ActionChains(browser)
-
-    elem = browser.find_element_by_tag_name("body")
-
-    count = 0
-    while True:
-        elem.send_keys(Keys.PAGE_DOWN)
-        time.sleep(0.2)
-        try:
-            elements = browser.find_elements_by_css_selector("div.entry a.see-all.no-line")
-            print("count", count, len(elements))
-            if count >= len(elements):
-                break
-
-            count = 0
-            for element in elements:
-                action.move_to_element(element)
-                action.click()
-                action.perform()
-                count += 1
-                time.sleep(0.1)
-        except Exception as ex:
-            print(ex)
 
     html = browser.page_source
     content = soup(html, "html.parser")
 
-    reviews = []
+    review_num = content.select("div.module-ibl-navi li.active")[0].text
+    review_num = re.findall(r'\d+', review_num)
 
-    review_list = content.find("div", {"class":"review-list"})
+    if len(review_num) > 0:
+        review_num = int(review_num[0])
+    else:
+        review_num = 0
 
-    if review_list:
-        review_items = review_list.find_all("div", {"class":"item review_item"})
-        for item in review_items:
-            review = {}
-            entry = item.find("div", {"class":"entry"})
-            review["title"] = normalize(entry.find("h4").text)
-            review["body"] = normalize(entry.find("div", {"class":"desc"}).text)
+    col_restaurant = db["hgw_restaurants"]
+    if review_num > 0:
+        overall_rating_container = content.find("div", {"class":"overall-col overall-total"})
+        star_view = overall_rating_container.find("div", {"class":"sprite star-view"})
+        rest_rating = get_rating_value(star_view)
+        col_restaurant.update_one({'_id':restaurant_id}, {"$set":{"rating":rest_rating}}, upsert=True)
+
+        no_of_pages = int(review_num / 5) + 1
+        url_pagination = url + "?sort=helpful_votes&page="
+
+        for i in range(1, no_of_pages+1):
+            browser.get(url_pagination+str(i))
+            elements = browser.find_elements_by_css_selector("div.entry a.see-all.no-line")
+            for element in elements:
+                browser.execute_script("arguments[0].click();", element)
+                time.sleep(0.3)
             
-            user_detail = entry.find("div", {"class":"user-detail"}).text.split("•")
-            user_detail = [x.strip() for x in user_detail]
-            review["user"] = user_detail[0]
-            review["date"] = user_detail[1]
+            html = browser.page_source.encode('utf-8')
+            content = soup(html, "html.parser")
+            review_list = content.find("div", {"class":"review-list"})
+            if review_list:
+                col_reviews = db["hgw_reviews"]
+                review_items = review_list.find_all("div", {"class":"item review_item"})
+                for item in review_items:
+                    review = {}
+                    entry = item.find("div", {"class":"entry"})
+                    review["title"] = normalize(entry.find("h4").text)
+                    review["body"] = normalize(entry.find("div", {"class":"desc"}).text)
+                    
+                    user_detail = entry.find("div", {"class":"user-detail"}).text.split("•")
+                    user_detail = [x.strip() for x in user_detail]
+                    review["user"] = user_detail[0]
+                    review["date"] = user_detail[1]
 
-            star_view = entry.find("div", {"class":"sprite star-view"})
-            rating = 0
-            if star_view.find("span", "sprite star-voted star-05"):
-                rating = 0.5
-            elif star_view.find("span", "sprite star-voted star-1"):
-                rating = 1
-            elif star_view.find("span", "sprite star-voted star-15"):
-                rating = 1.5
-            elif star_view.find("span", "sprite star-voted star-2"):
-                rating = 2
-            elif star_view.find("span", "sprite star-voted star-25"):
-                rating = 2.5
-            elif star_view.find("span", "sprite star-voted star-3"):
-                rating = 3
-            elif star_view.find("span", "sprite star-voted star-35"):
-                rating = 3.5
-            elif star_view.find("span", "sprite star-voted star-4"):
-                rating = 4
-            elif star_view.find("span", "sprite star-voted star-45"):
-                rating = 4.5
-            elif star_view.find("span", "sprite star-voted star-5"):
-                rating = 5
-
-            review["rating"] = rating
-            reviews.append(review)
-
-    return reviews
-
+                    star_view = entry.find("div", {"class":"sprite star-view"})
+                    if star_view:
+                        review["rating"] = get_rating_value(star_view)
+                        review["restaurant_id"] = restaurant_id
+                    
+                    col_reviews.update_one({'title':review['title']}, {"$set":review}, upsert=True)
+    else:
+        col_restaurant.update_one({'_id':restaurant_id}, {"$set":{"rating":0}}, upsert=True)
 
 def individual_page(url):
-    browser = get_browser()
-    browser.get(url)
-
-    try:
-        element = browser.find_element_by_css_selector("a.see-all.no-line.read-all")
-        element.click()
-    except Exception:
-        pass
+    db = client["KE5106"]
+    col = db["hgw_restaurants"]
     
-    html = browser.page_source
-    content = soup(html, "html.parser")
+    page = requests.get(url)
+    content = soup(page.content, "html.parser")
 
     data = {}
 
-    summary_container = content.find("div", {"class":"module-ibl-summary"})
-    data["title"] = normalize(summary_container.find("h1").text)
-    data["address"] = normalize(summary_container.find("p", {"class":"address"}).text)
-    data["description"] = normalize(summary_container.find("div", {"class":"des-view"}).text)
+    data["title"] = normalize(content.find("h1").text)
+    data["_id"] = data["title"]
+    data["address"] = normalize(content.find("p", {"class":"address"}).text)
+    data["description"] = normalize(content.find("div", {"class":"des-view"}).text)
 
-    try:
-        data["review_num"] = summary_container.find("span", {"class":"review-num"}).text
-    except Exception:
-        data["review_num"] = 0
-
-    info_detail = content.find("div", {"class":"info-detail"})
-    dl_elements = info_detail.find_all("dl", {"class":"dl-list"})
+    dl_elements = content.select("div.info-detail dl.dl-list")
 
     for dl in dl_elements:
         dt = dl.find("dt").text
@@ -141,17 +149,18 @@ def individual_page(url):
             val = dd.strip().replace("\n", " ")
             data[key] = val
 
+    col.update_one({'_id':data['_id']}, {"$set":data}, upsert=True)
+    
     review_page_url = url + "review/"
-    data["reviews"] = review_page(review_page_url)
-
-    return data
+    review_page(review_page_url, data['_id'])
+    print(data['_id'], "done!")
 
 def main():
     pp = pprint.PrettyPrinter(indent=2)
 
     url = base_url + "/restaurant-reservations/"
 
-    browser = get_browser()
+    browser = get_browser("chrome")
     browser.get(url)
 
     html = browser.page_source
@@ -167,8 +176,11 @@ def main():
         li_elements = ul.find_all("li") 
         for li in li_elements:
             a_element = li.find("a")
-            pp.pprint(individual_page(base_url + a_element['href']))
-            print("")
+            # individual_page("https://www.hungrygowhere.com/singapore/8-korean-bbq-scotts-road-newton/")
+            if count >= 14:
+                individual_page(base_url + a_element['href'])
+            # pp.pprint(individual_page(base_url + a_element['href']))
+            # print("")
             count += 1
             if count == limit:
                 return
